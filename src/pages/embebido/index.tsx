bdb3daa7b289bcc87ed0b6811614f4862c163cc3
Enrique Gil <35854635+enriquejgilq@users.noreply.github.com>
2023-03-16 17:16:14 -0500

    Update index.tsx

--- src/pages/embebido/index.tsx
+++ src/pages/embebido/index.tsx
@@ -5,7 +5,7 @@ function Embebido() {
         <div style={{ display: 'flex', width: '1000px', height: '1000px' }}>
             <iframe id="inlineFrameExample"
                 title="Inline Frame Example"
-                width="1000"
+                width="1300"
                 height="1000"
                 src="https://2ae4-186-29-122-155.ngrok.io/clients?code=hmOEG65N7BkY">
             </iframe>

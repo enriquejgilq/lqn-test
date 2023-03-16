583c2be32d2d4593ce6f31665184e963ed31b49b
Enrique Gil <35854635+enriquejgilq@users.noreply.github.com>
2023-03-16 16:55:12 -0500

    Update index.tsx

--- src/pages/embebido/index.tsx
+++ src/pages/embebido/index.tsx
@@ -7,7 +7,7 @@ function Embebido() {
                 title="Inline Frame Example"
                 width="1000"
                 height="1000"
-                src="https://dev.loquenecesito.co/clients?code=w11mX9EDGGmC">
+                src="https://2ae4-186-29-122-155.ngrok.io/clients?code=hmOEG65N7BkY">
             </iframe>
 
 

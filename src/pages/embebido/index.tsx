aefcad9a4feae7e0d975ca111c48ffb91950ccf1
Enrique Gil <35854635+enriquejgilq@users.noreply.github.com>
2023-03-16 17:18:38 -0500

    Update index.tsx

--- src/pages/embebido/index.tsx
+++ src/pages/embebido/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Embebido() {
     return (
-        <div style={{ display: 'flex', width: '1000px', height: '1000px' }}>
+        <div style={{ display: 'flex', width: '100%', height: '1000px' }}>
             <iframe id="inlineFrameExample"
                 title="Inline Frame Example"
                 width="1300"
